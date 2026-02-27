3e111e50f76fcc9416f1825aca5f761b11aa0f65
Patolinogay <p4toganso31@gmail.com>
2026-02-27 14:53:18 -0300

    conclusão da calculadora

--- PROJETOS/calculadora/calculadora.js
+++ PROJETOS/calculadora/calculadora.js
@@ -1,9 +1,19 @@
-function adicionar(value){
-    let exp = document.getElementById("display").value
+const visor = document.getElementById('display')
 
+function adicionar(valor){
+    visor.value += valor
 }
 
 function limpar(){
+    visor.value = ''
+}
 
-    let C = 
+function calcular(){
+try{
+    visor.value = new Function('return ' + visor.value )()
+}
+catch{
+    visor.value = "erro"
+    setTimeout(limpar, 1500)
+    }
 }
